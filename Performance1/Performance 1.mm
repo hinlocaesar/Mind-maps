2f3547c21188c01642b97fb0135696858756380f
Caesar <caesar.hinlo.tech@gmail.com>
2021-04-19 13:18:01 +0800

    Submit Notes for Network and Frontend Optimization

--- Performance1/Performance 1.mm
+++ Performance1/Performance 1.mm
@@ -8,7 +8,17 @@
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <node CREATED="1618744341540" ID="ID_561353197" MODIFIED="1618784871608" TEXT="Network">
 <font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1618746162035" ID="ID_1076504840" MODIFIED="1618746169940" TEXT="Honey I shrunk the files">
+<node CREATED="1618746162035" ID="ID_1076504840" MODIFIED="1618789370593">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Honey I shrunk the files<br />&quot;Minimizing files&quot;<br />
+    </p>
+  </body>
+</html></richcontent>
 <node CREATED="1618746224366" ID="ID_681568615" MODIFIED="1618746243279" TEXT="Minimize Text HTML, CSS, JS">
 <node CREATED="1618746316738" ID="ID_1579980676" MODIFIED="1618746435247" TEXT="OLD WAY: Use Uglify JS to minify code"/>
 <node CREATED="1618746413729" ID="ID_1608679636" MODIFIED="1618746442662" TEXT="NEW WAY: webpack is not use to minify during build"/>
@@ -35,7 +45,17 @@
 </node>
 </node>
 </node>
-<node CREATED="1618746171954" ID="ID_1473454473" MODIFIED="1618785381354" TEXT="The traveling deliveryman">
+<node CREATED="1618746171954" ID="ID_1473454473" MODIFIED="1618789391396">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      The traveling deliveryman<br />&quot;Minimizing delivery&quot;<br />
+    </p>
+  </body>
+</html></richcontent>
 <node CREATED="1618785174851" ID="ID_1964527964" MODIFIED="1618785284944" TEXT="Flexbox instead of bootstrap ask yourself :)"/>
 <node CREATED="1618785232167" ID="ID_900715122" MODIFIED="1618785256501" TEXT="Find libraries that are lightweight better use internal features"/>
 <node CREATED="1618785268000" ID="ID_878461349" MODIFIED="1618785276217" TEXT="Minimize # of images"/>
@@ -47,13 +67,63 @@
 <node CREATED="1618785468907" ID="ID_98003718" MODIFIED="1618785477439" TEXT="Combine Css Files and JS files into 1"/>
 </node>
 </node>
-<node CREATED="1618785719288" ID="ID_199003842" MODIFIED="1618785790650" TEXT="Use Chrome dev tools -network tab on brower to check behavior"/>
 </node>
 <node CREATED="1618744350515" ID="ID_1403800176" MODIFIED="1618784876208" TEXT="Backend">
 <font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
 </node>
 <node CREATED="1618744326274" ID="ID_854791731" MODIFIED="1618784878232" TEXT="Frontend">
 <font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1618790386037" ID="ID_678541943" LINK="https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn-media-1.freecodecamp.org%2Fimages%2Fecrsi9JGRA-uLZxs1ojHe4eJmyig79eJr3Dj&amp;imgrefurl=https%3A%2F%2Fwww.freecodecamp.org%2Fnews%2Fan-introduction-to-web-performance-and-the-critical-rendering-path-ce1fb5029494%2F&amp;tbnid=F5HpO-gAQzxUlM&amp;vet=12ahUKEwimj7-cgInwAhVMz4sBHQqtBkIQMygDegUIARCpAQ..i&amp;docid=sF6w2D11hGy4nM&amp;w=800&amp;h=530&amp;q=critical%20render%20path&amp;ved=2ahUKEwimj7-cgInwAhVMz4sBHQqtBkIQMygDegUIARCpAQ" MODIFIED="1618790454015" TEXT="Critical Render Path">
+<node CREATED="1618791302334" ID="ID_450908816" MODIFIED="1618798494429" TEXT="Path 1">
+<node CREATED="1618791013793" ID="ID_1478836838" MODIFIED="1618791025523" TEXT="Load CSS files at soon as possible"/>
+<node CREATED="1618791031766" ID="ID_783501525" MODIFIED="1618791040684" TEXT="Load JS as late as possible"/>
+</node>
+<node CREATED="1618798483312" ID="ID_461989841" MODIFIED="1618798486158" TEXT="Path 2">
+<node CREATED="1618798500930" ID="ID_1886017649" MODIFIED="1618798506482" TEXT="CSS is render blocking"/>
+</node>
+<node CREATED="1618800550270" ID="ID_1723904344" MODIFIED="1618800553396" TEXT="Path 3">
+<node CREATED="1618801260051" ID="ID_668840565" MODIFIED="1618801361845" TEXT="I can access both the DOM and CSSOM"/>
+<node CREATED="1618801461710" ID="ID_1883845253" MODIFIED="1618801467861" TEXT="JS is parser blocking"/>
+</node>
+<node CREATED="1618798560448" ID="ID_638389886" MODIFIED="1618801516510" TEXT="Solutions">
+<node CREATED="1618798583760" ID="ID_343136845" MODIFIED="1618798586783" TEXT="HTML">
+<node CREATED="1618798586786" ID="ID_1656182479" MODIFIED="1618798596318" TEXT="Load Style tag in the head"/>
+<node CREATED="1618798597048" ID="ID_1186338897" MODIFIED="1618805356822" TEXT="Load script right after body"/>
+</node>
+<node CREATED="1618798608951" ID="ID_1052143892" MODIFIED="1618798610500" TEXT="CSS">
+<node CREATED="1618798611121" ID="ID_489288334" MODIFIED="1618798621508" TEXT="Only load whatever is eneeded."/>
+<node CREATED="1618798632753" ID="ID_1036619264" MODIFIED="1618798643602" TEXT="Above the fold loading">
+<node CREATED="1618799127585" ID="ID_353333409" MODIFIED="1618800344978" TEXT="document.createstylesheet &amp; window.onload"/>
+</node>
+<node CREATED="1618798646423" ID="ID_1146609599" MODIFIED="1618799991826" TEXT="Media Attributes">
+<node CREATED="1618799876327" ID="ID_447245300" MODIFIED="1618799903657" TEXT="at head tag"/>
+</node>
+<node CREATED="1618798653880" ID="ID_300259328" MODIFIED="1618798660425" TEXT="Les Specificity"/>
+</node>
+<node CREATED="1618801256806" ID="ID_1925640639" MODIFIED="1618801259015" TEXT="JS">
+<node CREATED="1618801471891" ID="ID_74874609" MODIFIED="1618801479631" TEXT="Load Scripts asynchorounsly">
+<node CREATED="1618801762356" FOLDED="true" ID="ID_108615650" MODIFIED="1618801774223" TEXT="&lt;script&gt;">
+<node CREATED="1618801765916" ID="ID_1442225828" MODIFIED="1618801765916" TEXT=""/>
+</node>
+<node CREATED="1618801782908" ID="ID_1560988415" MODIFIED="1618802473215" TEXT="&lt;script async&gt;">
+<node CREATED="1618801930029" ID="ID_816882920" MODIFIED="1618802551784" TEXT="rule: add them that dones not affect the dom and cssom eg) Google anylytics scripts eg) 3rd party scripts"/>
+<node CREATED="1618802476828" ID="ID_1996839187" MODIFIED="1618802494111" TEXT="rule: if core functionaly require js use this"/>
+</node>
+</node>
+<node CREATED="1618801479868" ID="ID_769234615" MODIFIED="1618801486919" TEXT="Defer Loading of scripts">
+<node CREATED="1618801790292" ID="ID_1551478268" MODIFIED="1618801795974" TEXT="&lt;script defer&gt;">
+<node CREATED="1618802507795" ID="ID_1934944974" MODIFIED="1618802545874" TEXT="rule: if core functionaly require js use this eg) 3rd party script"/>
+</node>
+</node>
+<node CREATED="1618801487538" ID="ID_234998022" MODIFIED="1618801494495" TEXT="Minimize DOM manipulation"/>
+<node CREATED="1618801495746" ID="ID_265814634" MODIFIED="1618801503738" TEXT="Avoild long running Javascript"/>
+</node>
+</node>
+</node>
+<node CREATED="1618804544997" ID="ID_1102567829" MODIFIED="1618804772960" TEXT="Performance Test Tools">
+<node CREATED="1618804710793" ID="ID_1330992884" MODIFIED="1618804723826" TEXT="pagespeed insights"/>
+<node CREATED="1618804732079" ID="ID_1948006500" MODIFIED="1618804736565" TEXT="webpagetest"/>
+</node>
 </node>
 </node>
 </node>
