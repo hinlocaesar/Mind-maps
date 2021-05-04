a4b8c4ee5f005c867a83350937e176c83ff43253
Caesar <caesar.hinlo.tech@gmail.com>
2021-05-04 16:17:02 +0800

    Added note from Academia mind

---
+++ CSS Notes/CSS properties/position property css Academia Mind.mm
@@ -0,0 +1,39 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1620098977620" ID="ID_755876615" MODIFIED="1620099023702" TEXT="CSS properties">
+<node CREATED="1620103535156" ID="ID_1066867444" MODIFIED="1620103540215" POSITION="right" TEXT="What do we change?">
+<node CREATED="1620103553475" ID="ID_1883570873" MODIFIED="1620103558816" TEXT="We change the document flow"/>
+<node CREATED="1620103559867" ID="ID_1467418405" MODIFIED="1620103668164" TEXT="We change the positioning context //mean starting point"/>
+<node CREATED="1620103690692" ID="ID_1898354424" MODIFIED="1620103690692" TEXT="">
+<node CREATED="1620103702464" ID="ID_943675081" MODIFIED="1620103702464">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="position property css Academia Mind_5168037740735467645.jpeg" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1620103687358" ID="ID_95188068" MODIFIED="1620103687358" POSITION="right" TEXT=""/>
+<node CREATED="1620103239340" ID="ID_1400843823" MODIFIED="1620103272490" POSITION="right" TEXT="Change the position of an element in a website"/>
+<node CREATED="1620103381051" ID="ID_618499502" MODIFIED="1620103385224" POSITION="right" TEXT="1) Static">
+<node CREATED="1620103330939" ID="ID_1294150453" MODIFIED="1620103389591" TEXT=" static is the default position value applied to an element">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1620103390940" ID="ID_176801216" MODIFIED="1620103399923" TEXT="moving elements is impossible"/>
+<node CREATED="1620103408203" ID="ID_258348364" MODIFIED="1620103617182" TEXT="we position elements  based on the  document flow"/>
+</node>
+<node CREATED="1620103444925" ID="ID_198403615" MODIFIED="1620103459632" POSITION="left" TEXT="2) Absolute">
+<node CREATED="1620104067892" ID="ID_189907638" MODIFIED="1620104086427" TEXT="We exculeded the element from the normal workflow"/>
+</node>
+<node CREATED="1620103460412" ID="ID_1811709053" MODIFIED="1620103466433" POSITION="left" TEXT="3)Relative"/>
+<node CREATED="1620103467116" ID="ID_890026174" MODIFIED="1620103470269" POSITION="left" TEXT="4)Fixed"/>
+<node CREATED="1620103480156" ID="ID_1332388646" MODIFIED="1620103491745" POSITION="right" TEXT="6)Sticky">
+<node CREATED="1620103492707" ID="ID_1141104104" MODIFIED="1620103500106" TEXT="Hybrid of absolute and fixed"/>
+</node>
+</node>
+</map>
